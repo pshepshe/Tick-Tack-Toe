71566d5bc8714ce107819a21bc0899d9f6fcc3a0
pshepshe <71153364+pshepshe@users.noreply.github.com>
2020-10-19 21:40:13 +0300

    created class constructor

---
+++ tick-tack-toe.py
@@ -0,0 +1,5 @@
+class tick_tack:
+    def __init__(self, player1_name, player2_name):
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.current_step = 1
